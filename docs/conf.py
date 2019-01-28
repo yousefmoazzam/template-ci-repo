31209d86db043513a7589b8ccf4389ae9b843c34
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2019-01-28 15:07:21 +0000

    Edit conf.py
    Remove some unnecessarily repeated strings in conf.py and replace them
    with variables.

--- docs/conf.py
+++ docs/conf.py
@@ -21,9 +21,9 @@ import os
 
 # -- Project information -----------------------------------------------------
 
-project = u'Project'
-copyright = u'2019, Author'
+project = 'Project'
 author = u'Author'
+copyright = u'2019, ' + author
 
 # The short X.Y version
 version = u''
@@ -128,7 +128,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'Projectdoc'
+htmlhelp_basename = project + 'doc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -155,8 +155,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'Project.tex', u'Project Documentation',
-     u'Author', 'manual'),
+    (master_doc, project + '.tex', project + ' Documentation',
+     author, 'manual'),
 ]
 
 
@@ -165,7 +165,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'project', u'Project Documentation',
+    (master_doc, project,  project + ' Documentation',
      [author], 1)
 ]
 
@@ -176,8 +176,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'Project', u'Project Documentation',
-     author, 'Project', 'One line description of project.',
+    (master_doc, project, project + ' Documentation',
+     author, project, 'One line description of project.',
      'Miscellaneous'),
 ]
 
